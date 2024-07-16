7add7b66a1465ae60994f71ca5522d1309072707
Toky Yves <toky.r@nosytech.fr>
2024-07-16 11:31:32 +0300

    init jest test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,13 @@
-import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'angular-ci-app' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-ci-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-ci-app');
+  describe('AppComponent', () => {
+    let fixture: AppComponent;
+    beforeEach(() => {
+      fixture = new AppComponent();
+    });
+    it('add two numbers', () => {
+      expect(fixture.sum(1,4)).toBe(5);
+    });
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-ci-app';
+  sum(a: number, b: number) {
+    return a + b;
+}
 }
